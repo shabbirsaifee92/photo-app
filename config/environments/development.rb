36a6dfd8e08561aa7327644c5c056a0fa981b3aa
shabbir <ssaifee@covermymeds.com>
2018-03-25 17:58:32 -0400

    add email confirmation for sign up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,10 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  #config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'localhost:3003' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
   # Show full error reports.
   config.consider_all_requests_local = true
 
